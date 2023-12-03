93519e309d2b73eb8bdc37bfe09eb6eadcdb174b
Jalal Amourgha <myproo100@gmail.com>
2023-12-03 23:16:21 +0100

    Fix Errors

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import Todo from "./Components/Todo";
 
 const App = () => {
--- src/Components/Todoitems.jsx
+++ src/Components/Todoitems.jsx
@@ -1,3 +1,5 @@
+import React from "react";
+
 const Todoitems = ({ no, display, text, setTodos }) => {
   const remove = (no) => {
     let data = JSON.parse(localStorage.getItem("todos"));
